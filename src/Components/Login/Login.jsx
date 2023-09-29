00381a015b0e32205f289ca5d097fffedb7eddd6
Adarsh jain <aadarshj0304@gmail.com>
2023-09-29 13:11:21 +0530

    add svg folder

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 import "./Login.css";
+import { ReactComponent as GoogleIcon } from "../../SVG_Icon/Google.svg";
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="container">
+      <div className="loginBox">
+        <div className="logo">logo</div>
+        <p>We are glad to see you again.</p>
+        <div id="googleBtn">
+          <span class="label">Sign in with:</span>
+          <div id="customBtn">
+            <span class="icon"></span>
+            <GoogleIcon />
+            <span class="buttonText">Google</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
